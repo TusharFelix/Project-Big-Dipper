1b1c9f088762a61055e9862f387c5cd59aadfb59
santhoshnkl3 <santhoshnkl3@gmail.com>
2019-08-21 17:35:42 +0530

    added compare menu

--- features/compare/compare.js
+++ features/compare/compare.js
@@ -1,18 +1,44 @@
 
-var count=0;
-var mobileList;
-function Compare(compareElement){
-    count+=1;
-    mobileList.push(compareElement);
-    if(count<=2){
-        console.log(compareElement);
-        if(count==2){
-            mobileCompare();
+var count = 0;
+var mobileNameArray = [];
+var imageArray = [document.getElementById('mobile-one'), document.getElementById('mobile-two')];
+var mobileName = [document.getElementById('mobile-name-one'), document.getElementById('mobile-name-two')]
+var compareButton = document.getElementById('compare-btn');
+
+function Compare(compareElement) {
+    count++;
+    console.log(mobileNameArray);
+    if (count <= 2) {
+        mobileNameArray.push(compareElement);
+        var imageLocation = findImagePath(compareElement);
+        var temp = count - 1;
+        console.log(count);
+        mobileName[temp].innerText = compareElement;
+        imageArray[temp].src = imageLocation;
+        imageArray[temp].alt = compareElement;
+        imageArray[temp].width = 50;
+        if (count == 2) {
+            compareButton.disabled = false;
         }
     }
-    else{
-        alert("Cannot Compare More than 2 Mobiles");
+    else {
+        window.alert("cannot compare more than two mobiles");
+        count = 0;
     }
+
 }
 
-function mobileCompare()
+function mobileCompare(mobileOne, mobileTwo) {
+
+}
+
+function findImagePath(compareElement) {
+    for (var brandType in brands) {
+        for (var iter = 0; iter < brands[brandType].length; iter++) {
+            if (brands[brandType][iter]['model'] == compareElement) {
+                return brands[brandType][iter]['imageUrl'];
+            }
+
+        }
+    }
+}
