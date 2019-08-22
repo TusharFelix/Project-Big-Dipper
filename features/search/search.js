66b8c68d8b4b711cb2955f09f0c3639a96466785
NivethaMegal <nivetha.ramesh.0599@gmail.com>
2019-08-22 13:17:24 +0530

    search-space updated

--- features/search/search.js
+++ features/search/search.js
@@ -7,8 +7,10 @@ const serachMobiles = function(searchText){
     var mobilenames=Object.keys(brands);
     mobileSearchcard.innerHTML="";
     let matches = mobilenames.filter(function(brand) {
+        searchText=searchText.replace(" ","");
         const regex=new RegExp(`^${searchText}`,'gi');
         brands[brand].forEach(function(element) {
+            var modelName=element.model.replace(" ","");
             if(searchText.length==0){
                 document.querySelector(".brands-section").style.display="block"; 
             }
@@ -16,16 +18,19 @@ const serachMobiles = function(searchText){
                 document.querySelector(".brands-section").style.display="none";
                 mobileSearchcard.appendChild(createMobilecard(element));
             }
-            else if((element.model.match(regex))){
+            else if((modelName.match(regex))){
+                console.log(modelName);
+                console.log(regex);
                 document.querySelector(".brands-section").style.display="none";
                 mobileSearchcard.appendChild(createMobilecard(element));
+                //console.log(element);
             }
             else if((brand.match(regex))){
                 document.querySelector(".brands-section").style.display="none";
                 mobileSearchcard.innerHTML="";
                 brands[brand].forEach(function(element){
                 mobileSearchcard.appendChild(createMobilecard(element));
-                console.log(element);
+                // console.log(element);
                 });
             }
             else{
@@ -34,10 +39,10 @@ const serachMobiles = function(searchText){
         });
     });
 }
-
 search.addEventListener('click',function(){serachMobiles(search.value)});
 submitButton.addEventListener('click',function(){serachMobiles(search.value)});
 search.addEventListener('keyup',function(event){
+    // console.log(event);
     if(event.keyCode === 13){
         serachMobiles(search.value);
     }
