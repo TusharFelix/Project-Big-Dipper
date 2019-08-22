787d838f594a567ad768e802c63932acd8010481
santhoshnkl3 <santhoshnkl3@gmail.com>
2019-08-22 12:04:46 +0530

    styling done for pop up model

--- features/compare/compare.js
+++ features/compare/compare.js
@@ -1,22 +1,26 @@
-
 var count = 0;
 var mobileNameArray = [];
 var imageArray = [document.getElementById('mobile-one'), document.getElementById('mobile-two')];
 var mobileName = [document.getElementById('mobile-name-one'), document.getElementById('mobile-name-two')]
 var compareButton = document.getElementById('compare-btn');
+var compareContainer = document.querySelector('.compare-container');
+var removeBtn = [document.getElementById('mobileRemoveBtnOne'),document.getElementById('mobileRemoveBtnTwo')];
 
-function Compare(compareElement) {
+if (count == 0) {
+    compareContainer.style.display = "none";
+}
+
+function Compare(compareBrand,compareElement,compareRam) {
     count++;
-    console.log(mobileNameArray);
     if (count <= 2) {
-       // mobileNameArray.push(compareElement);
-        var imageLocation = findImagePath(compareElement);
+        compareContainer.style.display = "flex";
+        var imageLocation = findImagePath(compareBrand,compareElement,compareRam);
         var temp = count - 1;
-        console.log(count);
         mobileName[temp].innerText = compareElement;
         imageArray[temp].src = imageLocation;
         imageArray[temp].alt = compareElement;
         imageArray[temp].width = 50;
+        removeBtn[temp].style.display = "block";
         if (count == 2) {
             compareButton.disabled = false;
         }
@@ -25,92 +29,80 @@ function Compare(compareElement) {
         window.alert("cannot compare more than two mobiles");
         count = 0;
     }
-
 }
 
 function mobileCompare(mobileOne, mobileTwo) {
 
 }
 
-function findImagePath(compareElement) {
-    for (var brandType in brands) {
-        for (var iter = 0; iter < brands[brandType].length; iter++) {
-            if (brands[brandType][iter]['model'] == compareElement) {
-                mobileNameArray.push(brands[brandType][iter]);
-                return brands[brandType][iter]['imageUrl'];
+function findImagePath(compareBrand,compareElement,compareRam) {
+    var compareRamNew = compareRam+" ";
+    var compareBrandNew = compareBrand.parentElement.childNodes[1].innerText;
+        for (var iter = 0; iter < brands[compareBrandNew].length; iter++) {
+            if (brands[compareBrandNew][iter]['model'] == compareElement && brands[compareBrandNew][iter]['ram'] == compareRamNew) {
+                mobileNameArray.push(brands[compareBrandNew][iter]);
+                return brands[compareBrandNew][iter]['imageUrl'];
             }
-
         }
     }
-}
+
 
 var myModal = document.getElementById("modal");
 var btn = document.getElementById("compare-btn");
 var span = document.getElementsByClassName("close")[0];
 
 function popUpModal() {
-  myModal.style.display = "block";
+    myModal.style.display = "block";
 
-  span.onclick = function() {
-    myModal.style.display = "none";
-  }
-
-    window.onclick = function(event) {
-        if (event.target == myModal) {
-          myModal.style.display = "none";
-        }
+    span.onclick = function () {
+        myModal.style.display = "none";
     }
-
-   compareMobileFeature(mobileNameArray[mobileNameArray.length-2],mobileNameArray[mobileNameArray.length-1]);
+    compareMobileFeature(mobileNameArray[mobileNameArray.length - 2], mobileNameArray[mobileNameArray.length - 1]);
 }
-
-
 
 var tableTemplate = document.querySelector('.table-template');
 var tableRow = document.querySelector('.table-template-row');
-
 var tableColumn = document.querySelector('.table-template-column');
-
 var myDiv = document.querySelector('.modal-content');
 
-function compareMobileFeature(mobile1,mobile2){
-var tableTemplateNode = tableTemplate.cloneNode(false);
-tableTemplateNode.classList.remove('table-template');
-for (var aa in mobile1){
-if (aa=='imageUrl'){
-var tableRowNode = tableRow.cloneNode(false);
-console.log(tableRowNode);
-var tableColumnNodeKey=tableColumn.cloneNode(false);
-tableColumnNodeKey.innerText='Image';
-tableRowNode.appendChild(tableColumnNodeKey);
-var tableColumnNode=tableColumn.cloneNode(true);
-tableColumnNode.innerHTML= "<img src="+mobile1[aa]+" >";
-tableRowNode.appendChild(tableColumnNode);
-var tableColumnNode1=tableColumn.cloneNode(true);
-tableColumnNode1.innerHTML= "<img src="+mobile2[aa]+" >";
-tableRowNode.appendChild(tableColumnNode1);
-tableTemplateNode.appendChild(tableRowNode);
+function compareMobileFeature(mobile1, mobile2) {
+    var tableTemplateNode = tableTemplate.cloneNode(false);
+    tableTemplateNode.classList.remove('table-template');
+
+    for (var aa in mobile1) {
+        if (aa == 'imageUrl') {
+            var tableRowNode = tableRow.cloneNode(false);
+            var tableColumnNodeKey = tableColumn.cloneNode(false);
+            //      tableColumnNodeKey.innerText = "";
+            tableRowNode.appendChild(tableColumnNodeKey);
+            var tableColumnNode = tableColumn.cloneNode(true);
+            tableColumnNode.innerHTML = "<img src=" + mobile1[aa] + " >";
+            tableRowNode.appendChild(tableColumnNode);
+            var tableColumnNode1 = tableColumn.cloneNode(true);
+            tableColumnNode1.innerHTML = "<img src=" + mobile2[aa] + " >";
+            tableRowNode.appendChild(tableColumnNode1);
+            tableTemplateNode.appendChild(tableRowNode);
+        }
+        else {
+            var tableRowNode = tableRow.cloneNode(false);
+            var tableColumnNodeKey = tableColumn.cloneNode(false);
+            tableColumnNodeKey.innerText = aa;
+            tableRowNode.appendChild(tableColumnNodeKey);
+            var tableColumnNode = tableColumn.cloneNode(false);
+            tableColumnNode.innerText = mobile1[aa];
+            tableRowNode.appendChild(tableColumnNode);
+            var tableColumnNode1 = tableColumn.cloneNode(false);
+            tableColumnNode1.innerText = mobile2[aa];
+            tableRowNode.appendChild(tableColumnNode1);
+            tableTemplateNode.appendChild(tableRowNode);
+        }
+    }
+    myDiv.appendChild(tableTemplateNode);
+    mobileNameArray.pop();
+    mobileNameArray.pop();
 }
-else{
-var tableRowNode = tableRow.cloneNode(false);
-console.log(tableRowNode);
-var tableColumnNodeKey=tableColumn.cloneNode(false);
-tableColumnNodeKey.innerText=aa;
-tableRowNode.appendChild(tableColumnNodeKey);
-var tableColumnNode=tableColumn.cloneNode(false);
-tableColumnNode.innerText=mobile1[aa];
-tableRowNode.appendChild(tableColumnNode);
-var tableColumnNode1=tableColumn.cloneNode(false);
-console.log(tableColumnNode);
-tableColumnNode1.innerText=mobile2[aa];
-tableRowNode.appendChild(tableColumnNode1);
-tableTemplateNode.appendChild(tableRowNode);
-}
-}
-myDiv.appendChild(tableTemplateNode);
-mobileNameArray.pop();
-mobileNameArray.pop();
+
+function removeMobile(arg1){
 }
 
 
-  
