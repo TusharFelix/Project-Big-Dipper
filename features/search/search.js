ed405304235243ca4158bb05b31601e2467b5020
NivethaMegal <nivetha.ramesh.0599@gmail.com>
2019-08-23 16:57:47 +0530

    search-box empty command updated

--- features/search/search.js
+++ features/search/search.js
@@ -7,7 +7,8 @@ function removeCards() {
         item.remove();
     });
 }
-function model_and_ram_search(list,element,regex){
+
+function model_and_ram_search(list, element, regex) {
     var modelName = element.model.replace(/ |[()]/g, "");
     var mobileRam = element.ram.replace(/ |[()]/g, "");
     if (mobileRam.match(regex)) {
@@ -19,7 +20,7 @@ function model_and_ram_search(list,element,regex){
     return list;
 }
 
-function match_text(brand,searchText,filteredObject){
+function match_text(brand, searchText, filteredObject) {
     var list = [];
     searchText = searchText.replace(/ |[()]/g, "");
     const regex = new RegExp(`${searchText}`, 'gi');
@@ -28,7 +29,7 @@ function match_text(brand,searchText,filteredObject){
             filteredObject[brand] = brands[brand];
         }
         else {
-            list= model_and_ram_search(list,element,regex);  
+            list = model_and_ram_search(list, element, regex);
         }
     });
     if (list.length != 0) {
@@ -38,15 +39,20 @@ function match_text(brand,searchText,filteredObject){
 }
 
 function searchMobiles(searchText) {
+    if (searchText.length == 0) {
+        alert("search box is empty");
+    }
     removeCards();
     var filteredObject = {};
     var mobilenames = Object.keys(brands);
-    mobilenames.filter(function (brand) {
-        filteredObject=match_text(brand,searchText,filteredObject);
+    mobilenames.forEach(function (brand) {
+        filteredObject = match_text(brand, searchText, filteredObject);
     });
     renderApp(filteredObject);
+    // if (Object.keys(filteredObject).length == 0) {
+    //     alert("No results found...");
+    // }
 }
-
 submitButton.addEventListener('click', function () { searchMobiles(search.value) });
 search.addEventListener('keyup', function (event) {
     if (event.keyCode === 13) {
