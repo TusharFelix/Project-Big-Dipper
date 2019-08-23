15c2d5c8b48e396fcf54fdb21b7fad5dd3749ff5
Ammu Shajan <ammushajanj@gmail.com>
2019-08-23 14:25:05 +0530

    clear selection added in filter

--- features/filter/filter.js
+++ features/filter/filter.js
@@ -1,22 +1,46 @@
 var inputElements = document.querySelectorAll("input.mycheck");
 // console.log(inputElements);
+for (let i = 0; i < Array.from(inputElements).length; i++) {
+  inputElements[i].addEventListener("click", e => generateconditions(e));
+}
 document.querySelector(".filter-submit").addEventListener("click", e => {
-  return filterBrand();
+  return filterMobiles();
 });
 
 var brand = Object.keys(brands);
 var length = Object.keys(brands).length;
 var ram = [];
 var j = 0;
+var filterConditions = [
+  {
+    filterType: "PRICE",
+    applicable: false,
+    selected: []
+  },
+  {
+    filterType: "RAM",
+    applicable: false,
+    selected: []
+  },
+  {
+    filterType: "BRAND",
+    applicable: false,
+    selected: []
+  }
+];
 var filteredArray = [];
 var selectedFilter = null;
 for (var i = 0; i < length; i++) {
-  brands[brand[i]].forEach(function(item) {
+  brands[brand[i]].forEach(function (item) {
     ram[j++] = item["ram"];
   });
 }
 ram.sort();
+// console.log(ram);
 var val = [...new Set(ram)];
+// console.log(val);
+// console.log(brand);
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -27,39 +51,73 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 };
 
-submitbutton.onclick = function() {
+submitbutton.onclick = function () {
   modal.style.display = "none";
 };
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
-
+clearbutton.onclick=function()
+{
+  var items = document.querySelectorAll(".filter-check");
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].type == 'checkbox')
+                items[i].checked = false;
+        }
+}
 function openTab(click, openTab) {
   var i, content, link;
   selectedFilter = openTab.toUpperCase();
-  
+  // console.log(selectedFilter);
   content = document.getElementsByClassName("content");
   for (i = 0; i < content.length; i++) {
     content[i].style.display = "none";
   }
-  links = document.getElementsByClassName("links");
-  for (i = 0; i < links.length; i++) {
-    links[i].className = links[i].className.replace(" active", "");
-  }
+  // links = document.getElementsByClassName("links");
+  // for (i = 0; i < links.length; i++) {
+  //   links[i].className = links[i].className.replace(" active", "");
+  // }
   document.getElementById(openTab).style.display = "block";
-  click.currentTarget.className += " active";
+  // click.currentTarget.className += " active";
+  document.getElementById("submitbutton").style.display = "block";
+  document.getElementById("clearbutton").style.display="block";
+  document.getElementById("displaytext").style.display = "none";
+
+}
+function generateconditions(e) {
+  // var currentIndex = null;
+  // currentIndex = filterConditions.findIndex(function(ele) {
+  //   return ele.filterType === selectedFilter;
+  // });
+  // // console.log(selectedEle);
+  // if (e.target.checked) {
+  //   filterConditions[currentIndex].selected.push(selectedEle);
+  //   filterConditions[currentIndex].applicable = true;
+  //   console.log(filterConditions);
+  // } else {
+  //   var removeIndex = filterConditions[currentIndex].selected.findIndex(
+  //     function(ele) {
+  //       return ele === selectedEle;
+  //     }
+  //   );
+  //   filterConditions[currentIndex].selected.splice(removeIndex, 1);
+  //   if (!filterConditions[currentIndex].selected.length) {
+  //     filterConditions[currentIndex].applicable = false;
+  //   }
+  //   console.log(filterConditions);
+  // }
 }
 var myArray = val;
 var ramArray = [];
@@ -71,7 +129,7 @@ for (let i = 0; i < myArray.length; i++) {
     test: a[2]
   });
 }
-var resultArray = ramArray.sort(function(a, b) {
+var resultArray = ramArray.sort(function (a, b) {
   return b.type < a.type ? -1 : 1;
 });
 var filteredRam = [];
@@ -95,17 +153,23 @@ var brandfiltercontainertemplate = document.querySelector(
 );
 for (i = 0; i < brand.length; i++) {
   var brandfiltercontainer = brandfiltercontainertemplate.cloneNode(true);
+  // var brandfiltercheck = brandfilterchecktemplate.cloneNode(true);
+  // var brandFilter=brandFiltertemplate.cloneNode(true);
   brandfiltercontainer.classList.remove("brand-container-temp");
   var brandfiltercheck = brandfiltercontainer.querySelector("input");
   var brandFilter = brandfiltercontainer.querySelector("label");
   brandfiltercheck.classList.remove("brand-filter-check-temp");
   brandFilter.classList.remove("brand-filter-temp");
+  // console.log(brandFilter);
   brandfiltercheck.innerText = brand[i];
   brandfiltercheck.setAttribute("id", "brand-id-" + i);
   brandFilter.innerText = brand[i];
   brandFilter.setAttribute("for", "brand-id-" + i);
   brandWrapper.appendChild(brandfiltercontainer);
+  // brandContainer.appendChild(brandFilter);
 }
+
+// console.log(filteredRam);
 var ramFiltertemplate = document.querySelector(".ram-filter-temp");
 var ramContainer = document.querySelector("#ram-container");
 var ramWrapper = document.querySelector("#ram");
@@ -113,28 +177,73 @@ var ramfilterchecktemplate = document.querySelector(".ram-filter-check-temp");
 var ramfiltercontainertemplate = document.querySelector(".ram-container-temp");
 for (i = 0; i < filteredRam.length; i++) {
   var ramfiltercontainer = ramfiltercontainertemplate.cloneNode(true);
+  // var ramfiltercheck = ramfilterchecktemplate.cloneNode(true);
+  // var ramFilter=ramFiltertemplate.cloneNode(true);
   ramfiltercontainer.classList.remove("ram-container-temp");
   var ramfiltercheck = ramfiltercontainer.querySelector("input");
   var ramFilter = ramfiltercontainer.querySelector("label");
   ramfiltercheck.classList.remove("ram-filter-check-temp");
   ramFilter.classList.remove("ram-filter-temp");
+  // console.log(ramFilter);
   ramfiltercheck.innerText = filteredRam[i];
   ramfiltercheck.setAttribute("id", "ram-id-" + i);
   ramFilter.innerText = filteredRam[i];
   ramFilter.setAttribute("for", "ram-id-" + i);
   ramWrapper.appendChild(ramfiltercontainer);
+  // ramContainer.appendChild(ramFilter);
 }
 
+const filterMobiles = () => {
+  // var mobileBrands = Object.keys(brands);
+  // let allMobiles = [];
+  // for (let itr = 0; itr < mobileBrands.length; itr++) {
+  //   for (let i = 0; i < brands[mobileBrands[itr]].length; i++) {
+  //     brands[mobileBrands[itr]][i].mobileType = mobileBrands[itr];
+  //     // console.log(mobileBrands[itr]);
+  //   }
+  //   allMobiles = [...allMobiles, ...brands[mobileBrands[itr]]];
+  // }
+  // for (let i = 0; i < filterConditions.length; i++) {
+  //   if (filterConditions[i].applicable === true) {
+  //     if (filteredArray.length === 0) {
+  //       for (let mobile = 0; mobile < allMobiles.length; mobile++) {
+  //         var subjectedText =
+  //           allMobiles[mobile].mobileType.charAt(0).toUpperCase() +
+  //           allMobiles[mobile].mobileType.substr(1).toLowerCase();
+  //         if (filterConditions[i].selected.includes(subjectedText)) {
+  //           filteredArray.push(allMobiles[mobile]);
+  //         }
+  //       }
+  //     } else {
+  //       filteredArray = [];
+  //       for (let mobile = 0; mobile < allMobiles.length; mobile++) {
+  //         var subjectedText =
+  //           allMobiles[mobile].mobileType.charAt(0).toUpperCase() +
+  //           allMobiles[mobile].mobileType.substr(1).toLowerCase();
+  //         if (filterConditions[i].selected.includes(subjectedText)) {
+  //           filteredArray.push(allMobiles[mobile]);
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  // console.log(filteredArray);
+  // createMobileBrand("Samsung",filteredArray);
+  filterBrand();
+
+  // var selectedEle = e.target.nextElementSibling.innerText;
+  // filerteredBrandsObj[selectedEle] = brands[selectedEle];
+};
 var filteredBrandsObj = {};
 
 function filterBrand() {
   filerteredBrandsObj = {};
   var selectedBrandEle = document.querySelectorAll(".brand-container");
   var checkedBrands = extractCheckedValues(selectedBrandEle);
-  if(checkedBrands.length == 0) {
+  if (checkedBrands.length == 0) {
     filerteredBrandsObj = Object.assign({}, brands);
   }
-  checkedBrands.forEach(function(mobileName) {
+  checkedBrands.forEach(function (mobileName) {
     filerteredBrandsObj[mobileName] = brands[mobileName];
   });
   applyFilter(filerteredBrandsObj);
@@ -149,17 +258,32 @@ function applyFilter(filteredBrands) {
     "ram"
   );
   newBrandObj = filterMobileProperties(selectedPriceEle, newBrandObj, "price");
-  for(var brand in newBrandObj) {
-    if(newBrandObj[brand].length == 0) {
+  for (var brand in newBrandObj) {
+    if (newBrandObj[brand].length == 0) {
       delete newBrandObj[brand];
     }
   }
   removeBrand();
   renderApp(newBrandObj);
+  // renderApp(filterMobileProperties(selectedBrandEle, filteredBrands, "ram"));
+
+  // var selectedRam = [];
+  // Array.from(selectedBrandEle).forEach(function(ele, ind) {
+  //   if(ind != 0) {
+  //     if(ele.querySelector(".ram-filter-check").checked) {
+  //       selectedRam.push(ele.querySelector('.ram-filter').innerText);
+  //     }
+  //   }
+  // });
+  // var NewFilteredArray = [];
+  // for(var mobile in filteredBrands) {
+  //   NewFilteredArray[mobile] = filteredBrands[mobile].filter(filterMobilesUsingSpecs("ram", selectedRam));
+  // }
+  // renderApp(NewFilteredArray)
 }
 
 function filterMobilesUsingSpecs(type, checkedValues) {
-  return function(a) {
+  return function (a) {
     if (type == "price") {
       for (var i = 0; i < checkedValues.length; i++) {
         var priceamount = checkedValues[i];
@@ -168,6 +292,7 @@ function filterMobilesUsingSpecs(type, checkedValues) {
         var min = parseInt(splitvalues[0]);
         var max = parseInt(splitvalues[1]);
       }
+      // console.log(min, max, a[type]);
       if (a[type] > min && a[type] < max) {
         return 1;
       } else {
@@ -194,16 +319,16 @@ function filterMobileProperties(propertyEle, brandObj, filteringType) {
 
 }
 
-function removeBrand(){
+function removeBrand() {
   var removeElement = document.querySelectorAll('.brand-card')
-  removeElement.forEach(function(item) {
+  removeElement.forEach(function (item) {
     item.remove();
   });
 }
 
 function extractCheckedValues(propertyEle) {
   var checkedValues = [];
-  Array.from(propertyEle).forEach(function(ele, ind) {
+  Array.from(propertyEle).forEach(function (ele, ind) {
     if (ind != 0) {
       if (ele.querySelector(".filter-check").checked) {
         checkedValues.push(ele.querySelector(".filter-label").innerText);
