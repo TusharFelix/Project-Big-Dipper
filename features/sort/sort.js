2279a5ea6ae8dc04092c9287fb33ba0998e1ce95
tusharsingh5268 <tusharsingh5268@gmail.com>
2019-08-23 15:37:03 +0530

    radio button added to the options and add function to clear

--- features/sort/sort.js
+++ features/sort/sort.js
@@ -1,42 +1,67 @@
+var deletebrand=JSON.stringify(brands);
+deletebrand=JSON.parse(deletebrand);
 var eventClass = document.getElementsByClassName("sort-pop")
-for(i=0;i<eventClass.length;i++){
+for (i = 0; i < eventClass.length; i++) {
 	eventClass[i].addEventListener("click", priceFunction);
 }
-function sortObjectByKey(key){
-	return function(a,b){
-		if(typeof a[key] == "number" ){
-			if(a[key]>b[key])
+function sortObjectByKey(key) {
+	return function (a, b) {
+		if (typeof a[key] == "number") {
+			if (a[key] > b[key])
 				return 1;
 			return -1;
-		}else{
+		} else {
 			var dateReverse = a[key].split('-').reverse().join();
 			var dateSplit = b[key].split('-').reverse().join();
-			if(dateReverse>dateSplit)
+			if (dateReverse > dateSplit)
 				return 1;
 			return -1;
 		}
 	}
 }
-function removeBrand(){
+function removeBrand() {
 	var removeElement = document.querySelectorAll('.brand-card')
-	removeElement.forEach(function(item) {
-	item.remove();
+	removeElement.forEach(function (item) {
+		item.remove();
 	});
 }
-function sortBrands(key,order){
+function sortBrands(key, order) {
 	removeBrand();
-	for(brand in brands){
-		var sortedArray = brands[brand].sort(sortObjectByKey(key));
-		if(order=="desc"){
+	for (brand in deletebrand) {
+		var sortedArray = deletebrand[brand].sort(sortObjectByKey(key));
+		if (order == "desc") {
 			sortedArray.reverse();
 		}
-		createMobileBrand(brand,sortedArray);
+		createMobileBrand(brand, sortedArray);
 	}
 }
-		
-function priceFunction(event){
+
+function priceFunction(event) {
 	var itemType = event.target.getAttribute("value");
 	var order = event.target.getAttribute("data-order");
-	sortBrands(itemType,order);
+	if(itemType=="clear"){
+		removeBrand();
+		renderApp(brands);
+	}
+	else
+		sortBrands(itemType, order);
 
+}
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var openDropdown = dropdowns[0];
+		if (openDropdown.classList.contains('show')) {
+			openDropdown.classList.remove('show');
+		}
+
+	}
 }
